859b93a352f58542f4ddbc1bcfab758b71a29b71
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-03-31 14:39:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
-    historyApiFallback: true,  // This ensures proper routing in local development
+    historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true,
   }
 });
