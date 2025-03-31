34bcc93412f3f5a2a70ce44fe424b08bb8427206
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-03-31 11:13:48 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
-    proxy: {
-      "/api": {
-        target: "https://videostreming.onrender.com",
-        changeOrigin: true,
-      },
+    port: 3000,
+  },
+  // Add this for correct routing in production
+  resolve: {
+    alias: {
+      "@": "/src",
     },
   },
-  plugins: [react()],
+  base: "/",
 });
