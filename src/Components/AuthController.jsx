1d12d75d353d28d5793d09a75b8e3e824279b3f6
Rutvik Bansode <159120320+rutvik2026@users.noreply.github.com>
2025-04-23 21:43:40 +0530

    Update AuthController.jsx

--- src/Components/AuthController.jsx
+++ src/Components/AuthController.jsx
@@ -1,1 +1,33 @@
 
+// src/AuthContext.js
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [id, setId] = useState(null);
+
+  useEffect(() => {
+    const cust = sessionStorage.getItem("cust");
+    const { id } = cust ? JSON.parse(cust) : {};
+    setId(id);
+  }, []);
+
+  const login = (user) => {
+    sessionStorage.setItem("cust", JSON.stringify(user));
+    setId(user.id);
+  };
+
+  const logout = () => {
+    sessionStorage.removeItem("cust");
+    setId(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ id, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
